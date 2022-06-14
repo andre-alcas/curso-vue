596d707961874063b8e5c325db8d764b221c9bf8
andre-alcas <alcas.andre@gmail.com>
2022-06-14 13:44:28 -0300

    monsterkiller add

---
+++ monsterKiller/app.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#app',
+    data:{
+        running:false,
+        playerLife:100,
+        monsterLife:100,
+    },
+    computed:{
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0;
+        }
+
+    },
+    methods:{
+        startgame(){
+            this.running = true;
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        }
+
+    },
+    watch:{
+        
+    }
+   
+})

4c5b38747abf988253b1e5f20eb48062815f5cc6
andre-alcas <alcas.andre@gmail.com>
2022-07-11 17:31:51 -0300

    ajustes

---
+++ rotas/rotas-exercicios/src/router.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Inicio from './components/inicio/Inicio.vue'
+import Usuario from './components/usuario/Usuario.vue'
+
+Vue.use(Router)

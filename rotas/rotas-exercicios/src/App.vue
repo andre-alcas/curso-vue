3d636f38172103e3ce292396c3cf80c87a1f48d1
andre-alcas <alcas.andre@gmail.com>
2022-07-12 15:25:23 -0300

    ajustes

--- rotas/rotas-exercicios/src/App.vue
+++ rotas/rotas-exercicios/src/App.vue
@@ -12,9 +12,20 @@ export default {
 </script>
 
 <style>
+body {
+	margin: 0;
+}
+
 #app {
 	display: flex;
 	flex-direction: column;
+	height: 100%;
+	justify-content: center;
 	align-items: center;
+	text-align: center;
+
+	color: #fff;
+	background: linear-gradient(to right, rgba(83, 105, 118), rgb(41, 46, 73));
+
 }
 </style>
---
+++ routers/rotas/src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+import Panel from 'primevue/panel';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
+
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(router);
+
+app.component('Dialog', Dialog);
+app.component('Button', Button);
+app.component('Card', Card);
+app.component('Panel', Panel);
+
+app.mount('#app')

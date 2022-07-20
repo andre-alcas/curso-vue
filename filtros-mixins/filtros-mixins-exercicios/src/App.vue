592ae8d9b80d9cc44acbef3d437910681b0871d9
andre-alcas <alcas.andre@gmail.com>
2022-07-20 13:24:06 -0300

    ajustes

--- filtros-mixins/filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -1,12 +1,42 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ cpfDoAluno | cpf }}</p>
+		<input type="text" :value="cpfDoAluno | cpf" />
+		<hr>
+		<Frutas />
+		<hr>
+		<div>
+			<ul>
+				<li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+			</ul>
+			<input type="text" v-model="fruta" @keydown.enter="add" />
+		</div>
 	</div>
 </template>
 
 <script>
-export default {
+import frutasMixin from './frutasMixin'
+import Frutas from './Frutas.vue'
 
+export default {
+	components: { Frutas },
+	mixins: [frutasMixin],
+	filters: {
+		cpf(valor) {
+			const arr = valor.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	},
+	data() {
+		return {
+			cpfDoAluno: '12345678900',
+		}
+	},
 }
 </script>
 
@@ -18,6 +48,10 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+input {
 	font-size: 2.5rem;
 }
 </style>

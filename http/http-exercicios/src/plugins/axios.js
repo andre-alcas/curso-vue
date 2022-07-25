5bd3178dbd3fc976b74da4a630ce11c8015f568f
andre-alcas <alcas.andre@gmail.com>
2022-07-25 15:42:28 -0300

    ajustes

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -2,14 +2,36 @@ import Vue from 'vue'
 import axios from 'axios'
 
 //axios.defaults.baseURL = 'https://curso-vue-92d8d-default-rtdb.firebaseio.com/'
+//axios.defaults.headers.common['Authorization'] = 'abc 123'
+//axios.defaults.headers.common['Accepts'] = 'application/json'
 
 Vue.use({
     install(Vue) {
         //estará disponivel de forma global e isso aponta pro axios
         //Vue.prototype.$http = axios
         Vue.prototype.$http = axios.create({
-            baseURL: 'https://curso-vue-92d8d-default-rtdb.firebaseio.com/'
+            baseURL: 'https://curso-vue-92d8d-default-rtdb.firebaseio.com/',
+            headers: {
+                get: {
+                    'Authorization': 'abc 123'
+                }
+            }
 
         })
+        Vue.prototype.$http.interceptors.request.use(config => {
+            console.log(config.method)
+            /*             if (config.method == 'post') {
+                            config.method = 'put'
+                        } */
+            return config
+        }, error => Promise.reject(error))
+        Vue.prototype.$http.interceptors.response.use(res => {
+            /*  const array = []
+             for (let chave in res.data) {
+                 array.push({ id: chave, ...res.data[chave] })
+             }
+             res.data = array */
+            return res
+        }, error => Promise.reject(error))
     }
 })

5dfe23f60c5a4bd95b737bdc30fe9bd475f6dae0
andre-alcas <alcas.andre@gmail.com>
2022-07-25 15:42:00 -0300

    ajuestes

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-alert show dismissible v-for="mensagem in mensagens" :key="mensagem.texto" :variant="mensagem.tipo">
+			{{ mensagem.texto }}</b-alert>
 		<b-card>
 			<b-form-group label="Nome:">
 				<b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o nome">
@@ -20,6 +22,8 @@
 				<strong>Nome: </strong>{{ usuario.nome }}<br>
 				<strong>Email: </strong>{{ usuario.email }}<br>
 				<strong>ID: </strong>{{ id }}<br>
+				<b-button variant="warning" size="lg" @click="carregar(id)">Carregar</b-button>
+				<b-button variant="danger" size="lg" class="ml-2" @click="excluir(id)">Excluir</b-button>
 			</b-list-group-item>
 		</b-list-group>
 	</div>
@@ -35,7 +39,9 @@ export default {
 	  } */
 	data() {
 		return {
+			mensagens: [],
 			usuarios: [],
+			id: null,
 			usuario: {
 				nome: "",
 				email: "",
@@ -43,18 +49,47 @@ export default {
 		};
 	},
 	methods: {
-		salvar() {
-			this.$http.post('usuarios.json', this.usuario)
-				.then(resp => {
-					this.usuario.nome = ''
-					this.usuario.email = ''
+		limpar() {
+			this.id = null
+			this.usuario.nome = ''
+			this.usuario.email = ''
+			this.mensagens = []
+		},
+		carregar(id) {
+			this.id = id
+			this.usuario = { ...this.usuarios[id] }
+		},
+		excluir(id) {
+			this.$http.delete(`/usuarios/${id}.json`)
+				.then(() => this.limpar())
+				.catch(err => {
+					this.mensagens.push({
+						texto: 'Problema para excluir!',
+						tipo: 'danger'
+					})
 				})
+				.then(() => this.obterUsuarios())
+		},
+		salvar() {
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`/usuarios${finalUrl}`, this.usuario)
+				.then(() => {
+					this.limpar()
+					this.mensagens.push({
+						texto: 'Operação realizada com sucesso!',
+						tipo: 'success'
+					})
+				})
+				.then(() => this.obterUsuarios())
+			/* this.$http.post('usuarios.json', this.usuario)
+				.then(() => this.limpar()) */
 		},
 		obterUsuarios() {
 			this.$http('usuarios.json')
 				.then(res => {
 					this.usuarios = res.data
-					console.log(this.usuarios)
+					//console.log(this.usuarios)
 				})
 		}
 	}
